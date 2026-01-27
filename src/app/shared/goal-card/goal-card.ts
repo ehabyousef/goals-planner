e9fe8a8f5c266d12df97940f9929e3c1cbadff9e
ehabyousef <ehabyousef022@gmail.com>
2026-01-27 22:46:54 +0200

    feat: add navbar, sidebar, auth, goals, settings, tasks, goal-card, and task-card components with basic structure and tests

---
+++ src/app/shared/goal-card/goal-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-card',
+  imports: [],
+  templateUrl: './goal-card.html',
+  styleUrl: './goal-card.scss',
+})
+export class GoalCard {
+
+}
