8d9255f5580778993d6bd46e7bdc6ae5182d4173
ehabyousef <ehabyousef022@gmail.com>
2026-02-05 08:21:47 +0200

    feat(pagination): implement numbered pagination component with navigation controls
    - Added `HlmNumberedPagination` component for managing pagination with numbered links.
    - Created supporting components: `HlmPagination`, `HlmPaginationContent`, `HlmPaginationItem`, `HlmPaginationLink`, `HlmPaginationPrevious`, `HlmPaginationNext`, and `HlmPaginationEllipsis`.
    - Introduced utility functions for page array generation and out-of-bound correction.
    - Updated styles and structure for pagination elements.
    - Integrated pagination into the goals page with navigation controls.
    - Enhanced select options for filtering goals.
    - Updated global styles for improved theming and UI consistency.

--- src/app/pages/goals/goals.ts
+++ src/app/pages/goals/goals.ts
@@ -1,23 +1,49 @@
-import { Component } from '@angular/core';
+import { HlmPaginationImports } from './../../../../libs/ui/pagination/src/index';
+import { Component, signal } from '@angular/core';
 import { IGoal } from '../../core/interface/Types';
 import { GoalService } from '../../core/services/goal-service';
-
+import { Modal } from '../../components/modal/modal';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucidePlus } from '@ng-icons/lucide';
+import { HlmSelectImports } from '@spartan-ng/helm/select';
+import { BrnSelectImports } from '@spartan-ng/brain/select';
+import { DatePipe, NgClass } from '@angular/common';
 @Component({
   selector: 'app-goals',
-  imports: [],
+  imports: [
+    Modal,
+    HlmIcon,
+    NgIcon,
+    DatePipe,
+    NgClass,
+    HlmPaginationImports,
+    BrnSelectImports,
+    HlmSelectImports,
+    NgClass,
+  ],
   templateUrl: './goals.html',
   styleUrl: './goals.scss',
+  providers: [
+    provideIcons({
+      lucidePlus,
+    }),
+  ],
 })
 export class Goals {
   allProducts: IGoal[] = [];
   constructor(private _GoalService: GoalService) {}
+  goals = signal<IGoal[]>([]);
   ngOnInit(): void {
     this.AllGoals();
   }
 
   AllGoals() {
-    this._GoalService.getAllGoals().subscribe((next) => {
-      console.log(next);
+    this._GoalService.getAllGoals().subscribe({
+      next: (res) => {
+        this.goals.set(res.goals);
+        console.log(res);
+      },
     });
   }
 }
